7a6a9782e4e527f6b632bc2cf567b10607a8b3d0
puneetmahajan <puneet@persistence.one>
2023-09-15 15:53:16 +0530

    add chain-id to scripts

--- core-1/upgrades/v9.2.0/proposal-legacy.sh
+++ core-1/upgrades/v9.2.0/proposal-legacy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-persistenceCore tx gov submit-legacy-proposal software-upgrade "v9.2.0" \
+persistenceCore --chain-id core-1 tx gov submit-legacy-proposal software-upgrade "v9.2.0" \
     --deposit "512000000uxprt" \
     --upgrade-height 13275500 \
     --upgrade-info "https://raw.githubusercontent.com/persistenceOne/networks/core-1/upgrades/v9.2.0/core-1/upgrades/v9.2.0/binaries.json?checksum=sha256:d2c50e9bea0aab82ca8bd4d97a7b654a46fad061978def4c20df31624cb6352b" \
