7c56440d78e0d020ae123cc0351b45fbd45093d6
Max <maksim@persistence.one>
2023-09-15 12:48:08 +0300

    commit changes

--- test-core-2/upgrades/v9.2.0/proposal-legacy.sh
+++ test-core-2/upgrades/v9.2.0/proposal-legacy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-persistenceCore tx gov submit-legacy-proposal software-upgrade "v9.2.0" \
+persistenceCore tx --chain-id "test-core-2" gov submit-legacy-proposal software-upgrade "v9.2.0" \
     --deposit "600000000uxprt" \
     --upgrade-height 1668000 \
     --upgrade-info "https://raw.githubusercontent.com/persistenceOne/networks/test-core-2/upgrades/v9.2.0/test-core-2/upgrades/v9.2.0/binaries.json?checksum=sha256:d2c50e9bea0aab82ca8bd4d97a7b654a46fad061978def4c20df31624cb6352b" \
