22e735e13fba070fd0fef2014b2f1ffdc0c4d640
Max <maksim@persistence.one>
2023-09-15 12:48:08 +0300

    commit changes

--- test-core-2/upgrades/v9.2.0/proposal.sh
+++ test-core-2/upgrades/v9.2.0/proposal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-persistenceCore tx gov submit-proposal ./draft_proposal.json \
+persistenceCore tx --chain-id "test-core-2" gov submit-proposal ./draft_proposal.json \
     --node "https://rpc.testnet2.persistence.one:443" \
     --gas-adjustment 1.5 --gas auto --keyring-backend os --from testnet-super
