25e12a2ac06c062533bb71f793799bde4385d536
Ajeet Goyal <ajeetgoyal1997@gmail.com>
2023-12-20 23:46:55 +0530

    add sha256

--- test-core-2/upgrades/v11.1.0/proposal-legacy.sh
+++ test-core-2/upgrades/v11.1.0/proposal-legacy.sh
@@ -3,7 +3,7 @@
 persistenceCore tx --chain-id "test-core-2" gov submit-legacy-proposal software-upgrade "v11.1.0" \
     --deposit "600000000uxprt" \
     --upgrade-height 3137000 \
-    --upgrade-info "https://raw.githubusercontent.com/persistenceOne/networks/upgrade-v11.1.0/test-core-2/upgrades/v11.1.0/binaries.json?checksum=sha256:<TODO>" \
+    --upgrade-info "https://raw.githubusercontent.com/persistenceOne/networks/upgrade-v11.1.0/test-core-2/upgrades/v11.1.0/binaries.json?checksum=sha256:40eb91525b82935d81f707583e0e581a6845ffcde8df6aeb9aa63cf25480845d" \
     --title "Upgrade v11.1.0 PersistenceCore" \
     --description "This is a proposal to do a software upgrade to [v11.1.0](https://github.com/persistenceOne/persistenceCore/releases/tag/v11.1.0)" \
     --node "https://rpc.testnet2.persistence.one:443" \
