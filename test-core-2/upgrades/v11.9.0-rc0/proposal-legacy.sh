ed46dddf0efc5fa01010d2dc93be6095f3dcd4f7
kruspy <krusspy@gmail.com>
2024-04-05 15:12:46 +0200

    updates

--- test-core-2/upgrades/v11.9.0-rc0/proposal-legacy.sh
+++ test-core-2/upgrades/v11.9.0-rc0/proposal-legacy.sh
@@ -2,8 +2,8 @@
 
 persistenceCore tx --chain-id "test-core-2" gov submit-legacy-proposal software-upgrade "v11.9.0-rc0" \
     --deposit "600000000uxprt" \
-    --upgrade-height 4795000 \
-    --upgrade-info "https://raw.githubusercontent.com/persistenceOne/networks/test-core-2/upgrades/v11.9.0-rc0/test-core-2/upgrades/v11.9.0-rc0/binaries.json?checksum=sha256:85e1eda80321fae2fecc8fb005276b4019370181f6b6cc800dfedc432fc9f000" \
+    --upgrade-height 4795333 \
+    --upgrade-info "https://raw.githubusercontent.com/persistenceOne/networks/test-core-2/upgrades/v11.9.0-rc0/test-core-2/upgrades/v11.9.0-rc0/binaries.json?checksum=sha256:9551b5eae6f4de466b3506045347b7473c25933307dfb00804d3e28259d08d14" \
     --title "Upgrade v11.9.0-rc0 PersistenceCore" \
     --description "This is a proposal to do a software upgrade to [v11.9.0-rc0](https://github.com/persistenceOne/persistenceCore/releases/tag/v11.9.0-rc0)" \
     --node "https://rpc.testnet2.persistence.one:443" \
